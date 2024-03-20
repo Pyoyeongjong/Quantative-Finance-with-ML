f30b5c8169d5f91586a97f0e5b30bf1821a32559
Pyoyeongjong <pyjong1999@gmail.com>
2024-03-21 01:01:02 +0900

    Data Preparation & Try to do Linear_regression

--- Test.py
+++ Test.py
@@ -1,1 +1,11 @@
-print("Hello")
+import numpy as np
+
+x = np.array([1, 2])
+y = np.array([3, 5, 6])
+
+X, Y = np.meshgrid(x, y)
+print(X)
+
+percentiles=[.0001, .001, .01]
+percentiles+= [1-p for p in percentiles]
+print(percentiles)
